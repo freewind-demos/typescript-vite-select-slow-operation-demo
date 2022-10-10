9fef06a34b7797137afbb18960f14b8940b7cba8
Freewind <nowindlee@gmail.com>
2022-10-10 15:12:13 +0800

    typescript-vite-select-slow-operation-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript Vite React Demo
+TypeScript Vite Select Slow Operation Demo
 ===========================
+
+在select中进行一些慢的操作，看看会不会影响。果然会卡住
 
 ```
 npm install
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,29 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import './Hello.pcss';
 
 type Props = {};
 
+
 export const Hello: FC<Props> = ({}) => {
+  async function slowOperation() {
+    console.log("### 111")
+    await new Promise<void>((resolve) => {
+      const start = new Date().getTime();
+      while (true) {
+        if (new Date().getTime() - start > 3000) {
+          resolve()
+          return;
+        }
+      }
+    })
+    console.log("### 222")
+  }
+
   return <div className={'Hello'}>
-    <h1>Hello React</h1>
+    <select onChange={() => slowOperation()}>
+      <option value={111}>111</option>
+      <option value={222}>222</option>
+      <option value={333}>333</option>
+    </select>
   </div>;
 }
